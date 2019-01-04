149573849decf53481dc8f1e2199007182710300
eric_huang <eric_huang@trendmicro.com.cn>
2019-01-04 23:58:59 +0800

    upload RLE decode method

--- beginning_python/run-length-encoding.py
+++ beginning_python/run-length-encoding.py
@@ -28,6 +28,17 @@ def run_length_encode(text):
 
     # At this point, result looks like [('A', 3), ('B', 2), ...]
     # This next line combines the letters and counts and then puts everything into a single string
-    return ''.join('{}{}'.format(letter, count) for letter, count in result)
-print(run_length_encode('AAABBCCCDAAT'))
-assert run_length_encode('AAABBCCCDAAT') == 'A3B2C3D1A2T1'
+    #return ''.join('{}{}'.format(letter, count) for letter, count in result)
+    return result
+
+def run_length_decode(result):
+    string_result=''
+    for letter, count in result:
+        string_result += letter*count
+    return string_result
+
+result = run_length_encode('AAABBCCCDAAT')
+print(''.join('{}{}'.format(letter, count) for letter, count in result))
+print(run_length_decode(result))
+#assert run_length_encode('AAABBCCCDAAT') == 'A3B2C3D1A2T1'
+
