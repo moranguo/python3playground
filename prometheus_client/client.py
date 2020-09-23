e2e600d143db1f0148d9ec65c5c10476e4b09d49
huangzhiyong <huangzhiyong@360.cn>
2020-09-23 10:35:58 +0800

    prometheus python client

---
+++ prometheus_client/client.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+registry = CollectorRegistry()
+g = Gauge('ping', '检测最大响应时间',['dst_ip','city'], registry=registry) #Guage(metric_name,HELP,labels_name,registry=registry)
+g.labels('192.168.1.10','shenzhen').set(42.2) #set设定值
+g.labels('192.168.1.11','shenzhen').dec(2)  #dec递减2
+g.labels('192.168.1.12','shenzhen').inc()  #inc递增，默认增1
+push_to_gateway('10.217.61.103:9091', job='ping_status', registry=registry)
